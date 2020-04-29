6e7b40b34461551297508a606f05c0bad4cee545
Arely Viana <areviana17@gmail.com>
2020-04-28 21:15:14 -0600

    🎨 ⚡️Add user validations and avatar_url method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
-  validates :name, :email, presence: true
+  validates :full_name, :email, presence: true
+  validates_length_of :avatar, is: 122, if: -> { avatar.present? }
   has_one_attached :avatar
+
+  def avatar_url
+    if avatar.attached?
+      avatar.service_url
+      # S3_BUCKET.object(avatar.key).public_url
+    end
+  end
 end
