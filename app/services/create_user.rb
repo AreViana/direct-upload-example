4b6666704e70e18f4e081811b2e335324d34a672
Arely Viana <areviana17@gmail.com>
2020-04-28 21:14:26 -0600

    ✨ Add Create User service and attach avatar

---
+++ app/services/create_user.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class CreateUser < BaseService
+  def initialize(params)
+    @user_params = params.except(:avatar)
+    @avatar = params[:avatar]
+  end
+
+  def call
+    user = User.create!(user_params)
+    user.attach(avatar) if avatar.present? && !!user
+
+    user.as_json(root: false, methods: :avatar_url).except('updated_at')
+  end
+
+  private
+
+  attr_reader :user_params, :avatar
+end
