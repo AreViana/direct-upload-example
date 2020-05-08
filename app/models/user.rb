0487da2678d8b7ce3b39d96b74be15382934c8f7
Arely Viana <areviana17@gmail.com>
2020-05-08 15:51:03 -0600

    ⚡️Remove memory validations from model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 class User < ApplicationRecord
-  validates :full_name, :email, :avatar, presence: true
-  validates_length_of :avatar, is: 122
   validates :email, uniqueness: true
   has_one_attached :avatar
 
   def avatar_url
     if avatar.attached?
-      avatar.service_url
-      # S3_BUCKET.object(avatar.key).public_url
+      #avatar.service_url
+      S3_BUCKET.object(avatar.key).public_url
     end
   end
 end
